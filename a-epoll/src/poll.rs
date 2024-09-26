685ad5045d203e63026dbca121df517c5d49e575
Abeeujah <abeeujah@gmail.com>
2024-09-26 23:24:11 +0100

    Asynchronous Programming in Rust Part 2 - Chapter 4

---
+++ a-epoll/src/poll.rs
@@ -0,0 +1,67 @@
+use std::{
+    io::{self, Result},
+    net::TcpStream,
+    os::fd::AsRawFd,
+};
+
+use crate::ffi;
+
+type Events = Vec<ffi::Event>;
+
+pub struct Poll {
+    registry: Registry,
+}
+impl Poll {
+    pub fn new() -> Result<Self> {
+        let res = unsafe { ffi::epoll_create(1) };
+        match res < 0 {
+            true => Err(io::Error::last_os_error()),
+            false => Ok(Poll {
+                registry: Registry { raw_fd: res },
+            }),
+        }
+    }
+    pub fn registry(&self) -> &Registry {
+        &self.registry
+    }
+    pub fn poll(&mut self, events: &mut Events, timeout: Option<i32>) -> Result<()> {
+        let fd = self.registry.raw_fd;
+        let timeout = timeout.unwrap_or(-1);
+        let max_events = events.capacity() as i32;
+        let res = unsafe { ffi::epoll_wait(fd, events.as_mut_ptr(), max_events, timeout) };
+        match res < 0 {
+            true => Err(io::Error::last_os_error()),
+            false => unsafe {
+                events.set_len(res as usize);
+                Ok(())
+            },
+        }
+    }
+}
+
+pub struct Registry {
+    raw_fd: i32,
+}
+impl Registry {
+    pub fn register(&self, source: &TcpStream, token: usize, interests: i32) -> Result<()> {
+        let mut event = ffi::Event {
+            events: interests as u32,
+            epoll_data: token,
+        };
+        let op = ffi::EPOLL_CTL_ADD;
+        let res = unsafe { ffi::epoll_ctl(self.raw_fd, op, source.as_raw_fd(), &mut event) };
+        match res < 0 {
+            true => Err(io::Error::last_os_error()),
+            false => Ok(()),
+        }
+    }
+}
+impl Drop for Registry {
+    fn drop(&mut self) {
+        let res = unsafe { ffi::close(self.raw_fd) };
+        if res < 0 {
+            let err = io::Error::last_os_error();
+            println!("ERROR: {err:?}");
+        }
+    }
+}
